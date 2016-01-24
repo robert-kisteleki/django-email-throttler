f4db5f3022a5abade05d74ab68275f7219fa9a4c
robert@kad <kistel@kistel.hu>
2016-01-24 13:40:10 +0100

    clean up pypi install

--- setup.py
+++ setup.py
@@ -6,26 +6,24 @@ from setuptools import setup
 # Allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-# Get proper long description for package
-current_dir = dirname(abspath(__file__))
-
 # Get the long description from README.md
 setup(
     name="django-email-throttler",
-    version="0.2.5",
+    version="0.2.6",
     packages=["django_email_throttler"],
     include_package_data=True,
+
     license="GPLv3",
     description="An email throttler for Django",
     long_description="A Django email backend and more to limit the amount of mails sent out",
+
     url="https://github.com/robert-kisteleki/django-email-throttler",
     download_url="https://github.com/robert-kisteleki/django-email-throttler",
     author="Robert Kisteleki",
     author_email="kistel@gmail.com",
-    install_requires=[
-        "Django>=1.8",
-    ],
+
     keywords=['Django', 'email'],
+
     classifiers=[
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: POSIX",
